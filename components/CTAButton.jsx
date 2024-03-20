931cabcec70082fadb61182f9f2e17004ea37d92
Borislav Atanasov <120386696+borislavDci@users.noreply.github.com>
2024-03-21 00:37:18 +0100

    Add CTAButton component

---
+++ components/CTAButton.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+function CTAButton({ children, action = null }) {
+  return (
+    <button
+      onClick={() => (action ? action() : null)}
+      className="bg-[#3F8E00] px-16 py-5 rounded-md border-[1px] border-[#62BA1B] shadow-bg-CTA w-[300px] text-white text-shadow text-xl"
+    >
+      {children}
+    </button>
+  );
+}
+
+export default CTAButton;
