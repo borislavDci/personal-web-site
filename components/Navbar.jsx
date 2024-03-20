f1ddf1e15c57dea5804845e192d2a7df861f1370
Borislav Atanasov <120386696+borislavDci@users.noreply.github.com>
2024-03-21 00:37:39 +0100

    Refactor Navbar component to include additional icons and links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,28 +1,62 @@
 import Link from "next/link";
-import { FaLinkedinIn } from "react-icons/fa";
+import {
+  FaHome,
+  FaLinkedinIn,
+  FaProjectDiagram,
+  FaRProject,
+  FaToolbox,
+  FaTools,
+} from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
+import EmailClipboard from "./EmailClipboard";
 function Navbar() {
   return (
-    <div className="w-3/4 bg-[#1B1B1B] m-auto text-[#9C9C9C] rounded-b-lg py-8 flex justify-around items-center px-4 md:px-10 gap-5 flex-col md:flex-row">
-      <ul className="flex gap-10 md:justify-start justify-center w-full">
+    <div className="w-full sm:w-3/4 bg-[#1B1B1B] m-auto text-[#9C9C9C] rounded-b-lg py-8 flex justify-around items-center px-4 md:px-10 gap-5 flex-col md:flex-row">
+      <ul className="flex gap-10 md:justify-start justify-center w-full border-b-2 md:border-none uppercase pb-2">
         <li>
-          <Link href="/">Home</Link>
+          <Link className="hover:text-white flex items-center gap-2" href="#">
+            <FaHome /> Home
+          </Link>
         </li>
         <li>
-          <Link href="/">Home</Link>
+          <Link
+            className="hover:text-white flex items-center gap-2"
+            href="#skills"
+          >
+            <FaToolbox />
+            Skills
+          </Link>
         </li>
         <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/">Home</Link>
+          <Link
+            className="hover:text-white flex items-center gap-2"
+            href="#projects"
+          >
+            <FaProjectDiagram />
+            Projects
+          </Link>
         </li>
       </ul>
-      <div className="text-center text-4xl flex gap-5">
-        <FaLinkedinIn />
-        <FaGithub />
-        <MdEmail />
+      <div className="text-center text-4xl flex gap-5 relative px-5">
+        <a
+          className="hover:text-white"
+          href="https://www.linkedin.com/in/borislav-atanasov-60601624b/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedinIn />
+        </a>
+        <a
+          className="hover:text-white"
+          href="https://github.com/borislavDci"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+
+        <EmailClipboard />
       </div>
     </div>
   );
