b9f9b04666b2a9c7c2568de767ba580068ca1bc1
Borislav Atanasov <120386696+borislavDci@users.noreply.github.com>
2024-03-21 00:37:25 +0100

    Add EmailClipboard component

---
+++ components/EmailClipboard.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { MdEmail } from "react-icons/md";
+
+function EmailClipboard() {
+  const [isCoppied, setIsCoppied] = useState("");
+
+  useEffect(() => {
+    if (isCoppied === "Email copied to clipboard") {
+      setTimeout(() => {
+        setIsCoppied("");
+      }, 3000);
+    }
+  }, [isCoppied]);
+
+  return (
+    <>
+      <MdEmail
+        className="hover:cursor-pointer hover:text-white"
+        title="Copy email to clipboard"
+        onClick={() => {
+          if (isCoppied === "borislavm.atanasov@gmail.com") {
+            return setIsCoppied("");
+          }
+          if (!navigator.clipboard) {
+            return setIsCoppied("borislavm.atanasov@gmail.com");
+          }
+          navigator.clipboard.writeText("borislavm.atansov@gmail.com");
+          setIsCoppied("Email copied to clipboard");
+        }}
+      />
+      {isCoppied && (
+        <span className="text-white absolute top-10 left-0 text-sm">
+          {isCoppied}
+        </span>
+      )}
+    </>
+  );
+}
+
+export default EmailClipboard;
