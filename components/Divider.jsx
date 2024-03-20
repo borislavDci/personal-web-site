c9f125c3421ae3d0885774f2e02a866707e45156
Borislav Atanasov <120386696+borislavDci@users.noreply.github.com>
2024-03-21 00:37:22 +0100

    Add Divider component

---
+++ components/Divider.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Divider() {
+  return (
+    <div
+      className="my-10 h-2 w-[90%] rounded-sm m-auto bg-gradient-to-r
+from-green-300 from-0%%
+via-transparent via-50%
+to-purple-500 to-100%
+background-animate"
+    ></div>
+  );
+}
+
+export default Divider;
