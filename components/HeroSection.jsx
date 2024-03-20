cb202efe4f91f40c2f30e3d3d0db0cb95b6165c8
Borislav Atanasov <120386696+borislavDci@users.noreply.github.com>
2024-03-21 00:37:34 +0100

    Add HeroSection component

---
+++ components/HeroSection.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Divider from "./Divider";
+import CTAButton from "./CTAButton";
+
+function HeroSection() {
+  return (
+    <>
+      <div className="px-5 py-24 flex flex-col gap-5 items-center md:items-start md:w-3/4 m-auto">
+        <div className="flex items-center justify-between flex-col-reverse md:flex-row gap-10">
+          <div className="flex flex-col gap-8 w-full md:w-1/2">
+            <h1 className="text-4xl font-bold text-white ">
+              Borislav Atanasov
+            </h1>
+            <p className="text-[#9C9C9C]">
+              {`I'm a software engineer and I'm passionate about web development. I have
+          experience in building web applications with modern technologies like
+          React, Next.js, and Node.js. I'm also interested in learning new
+          technologies and frameworks.`}
+            </p>
+            <CTAButton>Download CV</CTAButton>
+          </div>
+          <Image
+            className="rounded-full"
+            src="/profile-image.webp"
+            width={300}
+            height={300}
+            alt="Borislav Atanasov's picture"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeroSection;
