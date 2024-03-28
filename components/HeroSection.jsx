5f663d4778e0023c2f1ba628a80204ab5469ba7b
Borislav Atanasov <120386696+borislavDci@users.noreply.github.com>
2024-03-28 22:51:10 +0100

    Update HeroSection component styling

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -7,7 +7,7 @@ function HeroSection() {
     <>
       <div className="px-5 py-24 flex flex-col gap-5 items-center md:items-start md:w-3/4 m-auto">
         <div className="flex items-center justify-between flex-col-reverse md:flex-row gap-10">
-          <div className="flex flex-col gap-8 w-full md:w-1/2">
+          <div className="flex flex-col gap-8 w-full md:w-1/2 items-start">
             <h1 className="text-4xl font-bold text-white ">
               Borislav Atanasov
             </h1>
@@ -17,7 +17,11 @@ function HeroSection() {
           React, Next.js, and Node.js. I'm also interested in learning new
           technologies and frameworks.`}
             </p>
-            <a className="max-w-max" href="/borislav-cv.pdf" download={true}>
+            <a
+              href="/borislav-cv.pdf"
+              className="self-center md:self-start"
+              download={true}
+            >
               <CTAButton>Download CV</CTAButton>
             </a>
           </div>
